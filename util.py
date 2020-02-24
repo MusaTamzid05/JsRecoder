bb1780ad97c6b6ac096dac7b08e5af2cdccc1bb3
MusaTamzid05 <tamzid101@gmail.com>
2020-02-24 23:03:38 +0600

    writing wav error fixed.

--- util.py
+++ util.py
@@ -1,14 +1,19 @@
 import wave
+import struct
 
-def write_data(data , path = "test.wav"):
+def write_data(audio_data , path = "test.wav"):
     sample_rate = 44100.0
     freg = 440.0
 
-    with wave.open(path , "w") as f:
+    with wave.open(path , "wb") as f:
         f.setnchannels(1)
         f.setsampwidth(2)
         f.setframerate(sample_rate)
-        f.writeframesraw(data)
+        f.writeframesraw(audio_data)
+
+        for data in audio_data:
+            current_data = struct.pack("<h" , data)
+            f.writeframesraw(current_data)
 
 
 
